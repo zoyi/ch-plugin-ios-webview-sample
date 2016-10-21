573316de79e8be7abf3cab47f9fc78ca1c69390d
Jenny Haeun Chung <jenny@zoyi.co>
2016-10-21 16:34:47 +0900

    Hide StatusBar

--- ch-plugin-ios-webview-sample/WebViewController.swift
+++ ch-plugin-ios-webview-sample/WebViewController.swift
@@ -12,14 +12,22 @@ class WebViewController: UIViewController, UIWebViewDelegate {
 
   @IBOutlet weak var webView: UIWebView!
 
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     print("viewDidLoad in WebViewController")
-
     self.openWebView()
   }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+
+
 
   private func openWebView() {
     let url = URL(string: "https://app.channel.io/plugins/b2b59139-f0cb-400d-b989-46a6793fdb3b")!
@@ -28,37 +36,3 @@ class WebViewController: UIViewController, UIWebViewDelegate {
   }
 }
 
-/*
- 
- For NSURLSession/NSURLConnection HTTP load failed (kCFStreamErrorDomainSSL, -9843) Error
- At Info.plist add -- security wise, may be bad
- <key>NSAppTransportSecurity</key>
- <dict>
- <key>NSAllowsArbitraryLoads</key>
- <true/>
- </dict>
-
- -- another option
- <key>NSAppTransportSecurity</key>
- <dict>
- <key>NSExceptionDomains</key>
- <dict>
- <key>myDomain.com</key>
- <dict>
- <!--Include to allow subdomains-->
- <key>NSIncludesSubdomains</key>
- <true/>
- <!--Include to allow HTTP requests-->
- <key>NSTemporaryExceptionAllowsInsecureHTTPLoads</key>
- <true/>
- <!--Include to specify minimum TLS version-->
- <key>NSTemporaryExceptionMinimumTLSVersion</key>
- <string>TLSv1.1</string>
- </dict>
- </dict>
- </dict>
-
-
-
-
- */
