edd6d77248c3a2441c092c649ab0f88406885fd6
Jenny Haeun Chung <jenny@zoyi.co>
2016-10-21 16:13:53 +0900

    Erase Info.plist NSAppTransportSecurity setting

--- ch-plugin-ios-webview-sample/WebViewController.swift
+++ ch-plugin-ios-webview-sample/WebViewController.swift
@@ -22,7 +22,7 @@ class WebViewController: UIViewController, UIWebViewDelegate {
   }
 
   private func openWebView() {
-    let url = URL(string: "http://www.naver.com")!
+    let url = URL(string: "https://app.channel.io/plugins/b2b59139-f0cb-400d-b989-46a6793fdb3b")!
     let request = URLRequest(url: url)
     self.webView.loadRequest(request)
   }
