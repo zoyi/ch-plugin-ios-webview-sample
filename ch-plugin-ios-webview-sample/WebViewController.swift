e93214d11258b854aedf5a1fde1dd130c966b1b5
Jenny Haeun Chung <jenny@zoyi.co>
2016-10-21 17:20:36 +0900

    Enable Photo Library & Camera

--- ch-plugin-ios-webview-sample/WebViewController.swift
+++ ch-plugin-ios-webview-sample/WebViewController.swift
@@ -12,22 +12,19 @@ class WebViewController: UIViewController, UIWebViewDelegate {
 
   @IBOutlet weak var webView: UIWebView!
 
-  override var prefersStatusBarHidden: Bool {
-    return true
-  }
+//  override var prefersStatusBarHidden: Bool {
+//    return true
+//  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    print("viewDidLoad in WebViewController")
     self.openWebView()
   }
 
   override func viewWillAppear(_ animated: Bool) {
+
     super.viewWillAppear(animated)
   }
-
-
 
   private func openWebView() {
     let url = URL(string: "https://app.channel.io/plugins/b2b59139-f0cb-400d-b989-46a6793fdb3b")!
